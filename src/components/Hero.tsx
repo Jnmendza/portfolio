e2200dd6bb2283a2fb13e26e94ee256b835fcc92
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-05-04 01:34:01 -0700

    added links

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { bebasFont } from "@/lib/font";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <div className='relative h-screen w-full overflow-hidden'>
-      {/* Background Text Container (centered) */}
       <div
         className={`${bebasFont.className} absolute inset-0 z-0 flex flex-col justify-center overflow-hidden`}
       >
@@ -23,7 +23,6 @@ const Hero = () => {
       <div className='absolute left-0 top-0 h-full w-1/3 z-1 bg-gradient-to-r from-black to-transparent pointer-events-none' />
       <div className='absolute right-0 top-0 h-full w-1/3 z-1 bg-gradient-to-l from-black to-transparent pointer-events-none' />
 
-      {/* Centered Content */}
       <div className='absolute inset-0 z-10 flex flex-col justify-center items-center text-center p-8 space-y-4'>
         <p className='text-3xl font-bold text-primary'>Hey There I&apos;m</p>
         <h1 className={`text-9xl ${bebasFont.className} text-primary`}>
@@ -33,14 +32,18 @@ const Hero = () => {
           Currently studying computer science and engineering.
         </p>
         <div className='flex space-x-10 mt-2 text-primary'>
-          <FaGithub
-            size={50}
-            className='cursor-pointer transform transition-transform duration-200 hover:scale-110'
-          />
-          <FaLinkedin
-            size={50}
-            className='cursor-pointer transform transition-transform duration-200 hover:scale-110'
-          />
+          <Link href={"https://github.com/Jnmendza"}>
+            <FaGithub
+              size={50}
+              className='cursor-pointer transform transition-transform duration-200 hover:scale-110'
+            />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/jnmendza/"}>
+            <FaLinkedin
+              size={50}
+              className='cursor-pointer transform transition-transform duration-200 hover:scale-110'
+            />
+          </Link>
         </div>
       </div>
     </div>
