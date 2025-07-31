7642f027f1116633e76402f92b2c7eb3db8b08a0
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-07-31 00:13:15 -0700

    config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "plus.unsplash.com",
-      },
+    domains: [
+      "plus.unsplash.com",
+      "qtmkwwvomuvavuoaqjcn.supabase.co",
+      "cdn.sanity.io",
     ],
   },
 };
